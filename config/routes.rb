2baf7f4dc6940e17b85443859cd280dfae314ba9
Jelena Rakic Saufley <jrjela@gmail.com>
2017-08-13 10:27:29 -0400

    adding home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#home'
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
